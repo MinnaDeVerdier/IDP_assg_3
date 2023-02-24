db03210aa518be3e373e07883a226fa666da5b60
MinnaDeVerdier <minna.deverdier@gmail.com>
2023-02-24 13:00:13 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Xml;
+
+namespace IDP_assg_3
+{
+    internal class Program
+    {
+        public class Element
+        {
+            public string namn = "";
+            public double Z = 0, smältpunkt = 0, kokpunkt = 0;
+            public bool typ;
+            public Element(string namn, double atomnummer, bool isMetall, double smältpunkt, double kokpunkt) 
+            { 
+                this.namn = namn;
+                this.Z = atomnummer;
+                this.typ = isMetall;
+                this.smältpunkt = smältpunkt;
+                this.kokpunkt=kokpunkt;
+            }
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
